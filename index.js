0d5b2c7515e4381ece6f29b1925711c1c08cd5a2
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-08 16:37:43 +0700

    tidy up code

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-var isEmpty = function (arr) {
-  return arr[0] === undefined
-}
+var isEmpty = function (arr) {return arr[0] === undefined}
 
 console.log(isEmpty([]))
 
-var count = function (arr, currCount = 0) {
-  return isEmpty(arr) ? currCount : count(arr.slice(1), currCount + 1)
-}
+var count = function (arr, currCount = 0) { return isEmpty(arr) ? currCount : count(arr.slice(1), currCount + 1) }
 
 console.log(count(['Danang', 'Aji', 'Tamtomo']))
 
-var head = function (arr) {
-  return arr[0]
-}
+var head = function (arr) { return arr[0]}
 
 console.log(head([1, 2, 3]))
 console.log(head([4, 5, 6]))
 
-var tail = function (arr) {
-  return arr.slice(1)
-}
+var tail = function (arr) {return arr.slice(1)}
 
 console.log(tail([1, 2, 3]))
 console.log(tail(2, 3))
