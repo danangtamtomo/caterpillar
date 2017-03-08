3b5c19e5d61ec81ff85aa8c787c5e36e1ad8f33a
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-08 16:58:19 +0700

    split lib and test code

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-var isEmpty = function (arr) {return arr[0] === undefined}
+var lib = require('./lib/lib')
 
-console.log(isEmpty([]))
+console.log(lib.isEmpty([]))
 
-var count = function (arr, currCount = 0) { return isEmpty(arr) ? currCount : count(arr.slice(1), currCount + 1) }
+console.log(lib.count(['Danang', 'Aji', 'Tamtomo']))
 
-console.log(count(['Danang', 'Aji', 'Tamtomo']))
+console.log(lib.head([1, 2, 3]))
+console.log(lib.head([4, 5, 6]))
 
-var head = function (arr) { return arr[0]}
+console.log(lib.tail([1, 2, 3]))
+console.log(lib.tail([2, 3]))
 
-console.log(head([1, 2, 3]))
-console.log(head([4, 5, 6]))
-
-var tail = function (arr) {return arr.slice(1)}
-
-console.log(tail([1, 2, 3]))
-console.log(tail(2, 3))
-
-var flatten = function (arr) {
-  return arr.reduce(function (acc, val) {
-    return acc.concat(Array.isArray(val) ? flatten(val) : val)
-  }, [])
-}
-
-console.log(flatten([1, [2, 3], 4, [5, [6, 7]]]))
+console.log(lib.flatten([1, [2, 3], 4, [5, [6, 7]]]))
